f995d92c942b0d9e5f906f2ca478675d0d7ca0a3
alexrevell <revellalex@gmail.com>
2015-05-15 20:08:35 +1200

    view stories and can generate new story from form

--- javascripts/story.js
+++ javascripts/story.js
@@ -20,3 +20,35 @@ Story.create = function(formData, success, failure){
   });
 }
 
+Story.getAll = function(){
+  $.ajax({
+      url: 'https://corpsebook-server.herokuapp.com/stories',
+      type: 'GET',
+      success: function(response){
+        htmlString = generateStory(response);
+        $('.story-list').html(htmlString);
+      },
+      error: function(status, error){
+        console.log("error");
+      }
+    });
+}
+
+function generateStory(story){
+  var htmlString =  '<div class="story-list-render">';
+      htmlString += '<h1>Story List</h1>';
+          for (var i = 0; i <story.length; i++) {
+              htmlString += '<ul class="render-story" style="border: 2px solid green; list-style: none;">';
+              htmlString += '<li>Title: '+ story[i].title +'</li>';
+              htmlString += '<li>Latitude: '+ story[i].origin_latitude +'</li>';
+              htmlString += '<li>Longitude: '+ story[i].origin_longitude +'</li>';
+              htmlString += '<li>Contribution Length: '+ story[i].contribution_limit +'</li>';
+              htmlString += '<li><p class="submit"><input type="submit" value="Contribute"></p></li>';
+              htmlString += '</ul>';
+          };
+
+      htmlString += '</div>';
+      return htmlString;
+
+}
+
