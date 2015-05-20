c746ee6b38b9fc1d4dd21d118512e2ad81ec6ac1
alexrevell <revellalex@gmail.com>
2015-05-20 13:48:17 +1200

    about to merge some css from Ira

--- javascripts/models/mapModel.js
+++ javascripts/models/mapModel.js
@@ -88,7 +88,6 @@ MapModel.prototype = {
     console.log("reverse Geo")
     var latlng = new google.maps.LatLng(lat, lng);
     this.geocoder.geocode({'latLng': latlng}, function(results, status) {
-      console.log(status)
         if(status == google.maps.GeocoderStatus.OK) {
             if(results[0]) {
                 callback(results[0].address_components[1].long_name);
